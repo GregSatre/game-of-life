bc6c8e049dea3811db82a4e1026403639b9e7ca6
GregSatre <greg.satre@gmail.com>
2014-01-11 14:54:01 +0100

    Added pause and frame by frame advance and return.

--- lifegame.py
+++ lifegame.py
@@ -6,7 +6,7 @@ WINDOW_HEIGHT = 600
 
 FPS = 5
 
-CELL_SIZE = 40
+CELL_SIZE = 30
 assert WINDOW_WIDTH%CELL_SIZE == 0
 assert WINDOW_HEIGHT%CELL_SIZE == 0
 CELL_WIDTH = WINDOW_WIDTH//CELL_SIZE
@@ -31,7 +31,7 @@ def main():
     # Get initial cell positions
     cell_positions = initializeGame()
     # Run game on those positions
-    runGame(cell_positions)
+    runGame(cell_positions, [])
 
 def initializeGame():
     """Checks for user mouse clicks and updates cell_positions accordingly. Returns game's initial state"""
@@ -64,7 +64,7 @@ def initializeGame():
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
-def runGame(cell_positions):
+def runGame(cell_positions, history):
     """Using initial state as input and a set of rules updates cell_positions and displays the result. Infinite loop"""
     while True:
         # Event handling loop
@@ -74,9 +74,12 @@ def runGame(cell_positions):
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     terminate()
+                elif event.key == K_SPACE:
+                    pauseGame(cell_positions, history)
                 elif event.key == K_r:
                     resetGame()
 
+        history.append(list(cell_positions))
         cell_positions = updateCells(cell_positions)
 
         SCREEN.fill(BGCOLOR)
@@ -89,11 +92,38 @@ def resetGame():
     # Get initial cell positions
     cell_positions = initializeGame()
     # Run game on those positions
-    runGame(cell_positions)
+    runGame(cell_positions, [])
+
+def pauseGame(cell_positions, history):
+    while True:
+        # Event handling loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()
+                elif event.key == K_SPACE:
+                    runGame(cell_positions, history)
+                elif event.key == K_RIGHT:
+                    history.append(list(cell_positions))
+                    cell_positions = updateCells(cell_positions)
+                elif event.key == K_LEFT:
+                    cell_positions = set(history.pop())
+                elif event.key == K_r:
+                    resetGame()
+
+        SCREEN.fill(BGCOLOR)
+        drawGrid()
+        drawCells(cell_positions)
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
 
 def updateCells(cell_positions):
     """Use rules of the game of life to update the cell positions"""
     # Build a set of canditates for live cells at the next generation, instead of looking through the whole grid
+    # These will be dead neighbours of living cells
     possible_future_cells = set()
     # Make sets of cells to add and remove at the end of the check
     cells_remove = set()
@@ -104,9 +134,10 @@ def updateCells(cell_positions):
         number_live_neighbours = 0
         # Check which of these corresponds to another living cell
         for square in neighbours_dict.values():
-            possible_future_cells.add(square)
             if square in cell_positions:
                 number_live_neighbours+=1
+            else:
+                possible_future_cells.add(square)
 
         # Any live cell with fewer than two live neighbours dies, as if caused by under-population
         if number_live_neighbours<2:
@@ -127,10 +158,10 @@ def updateCells(cell_positions):
         if count == 3:
             cells_add.add(cell_candidate)
     # Update cell_positions by removing dead cells and adding new-born cells
-    for cell in cells_remove:
-        cell_positions.remove(cell)
     for cell in cells_add:
         cell_positions.add(cell)
+    for cell in cells_remove:
+        cell_positions.remove(cell)
     # Return the update live cell list
     return cell_positions
 
@@ -154,20 +185,22 @@ def cellNeighbours((cell_x,cell_y)):
     return dict
 
 def drawGrid():
-    for x in range(0,WINDOW_WIDTH,CELL_WIDTH):
-        pygame.draw.line(SCREEN, DARKGRAY, (x,0), (x,WINDOW_HEIGHT))
-    for y in range(0,WINDOW_HEIGHT,CELL_HEIGHT):
-        pygame.draw.line(SCREEN, DARKGRAY, (0,y), (WINDOW_WIDTH,y))
+    for x in range(0,WINDOW_WIDTH+CELL_WIDTH,CELL_WIDTH):
+        pygame.draw.line(SCREEN, BLACK, (x,0), (x,WINDOW_HEIGHT))
+    for y in range(0,WINDOW_HEIGHT+CELL_HEIGHT,CELL_HEIGHT):
+        pygame.draw.line(SCREEN, BLACK, (0,y), (WINDOW_WIDTH,y))
 
 def drawCells(cell_positions):
     """Draws cells on the display"""
     for (cell_x, cell_y) in cell_positions:
-        rect = pygame.Rect(cell_x, cell_y, CELL_WIDTH, CELL_HEIGHT)
-        pygame.draw.rect(SCREEN, BLACK, rect)
+        cellRect = pygame.Rect(cell_x, cell_y, CELL_WIDTH, CELL_HEIGHT)
+        pygame.draw.rect(SCREEN, BLACK, cellRect)
 
 def inWindow((square_x, square_y)):
     """Returns True if the square is in the window. False otherwise"""
-    if 0<=square_x<=WINDOW_WIDTH and 0<=square_y<=WINDOW_HEIGHT:
+    # Take care in the conditions. Since cells are tracked by theur upper-left corner,
+    # there is a cell-wide gap on the right and bottom of the window
+    if 0<=square_x<=WINDOW_WIDTH-CELL_WIDTH and 0<=square_y<=WINDOW_HEIGHT-CELL_HEIGHT:
         return True
     else:
         return False
